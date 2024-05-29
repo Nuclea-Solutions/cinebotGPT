68a46984ce9af2b49b614c96327b35aad6ad2b7c
Marlon Espinosa <82923825+Marlonep@users.noreply.github.com>
2024-05-28 23:22:12 -0600

    Scrapper with GCloud Funciton

--- gcloudFunctions/createSession/index.js
+++ gcloudFunctions/createSession/index.js
@@ -1,41 +1,104 @@
-require('dotenv').config(); // Cargar variables de entorno desde el archivo .env
-var admin = require("firebase-admin");
+const puppeteer = require('puppeteer-core');
+const chromium = require('chrome-aws-lambda');
+const admin = require('firebase-admin');
+require('dotenv').config();
 
-// Cargar la clave de servicio desde la variable de entorno
-var serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+// Initialize Firebase Admin SDK
+const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://cinebot-7f35b-default-rtdb.firebaseio.com" // Opcional para Firestore
+  databaseURL: "https://cinebot-7f35b-default-rtdb.firebaseio.com"
 });
 
-var db = admin.firestore();
+const db = admin.firestore();
 
-// Datos para el documento en la colección 'sessions'
-var sessionData = {
-  movies: "https://example.com/movies",
-  showtimes: "https://example.com/showtimes"
+exports.scrapeCinepolis = async (req, res) => {
+    console.log('Scraping function called.');
+
+    const zoneValue = req.query.zoneValue || 'zona-35'; // Get zoneValue from query parameters, default to 'zona-35'
+    let value = zoneValue;
+
+    if (zoneValue.includes('zona-')) {
+        value = `area/${zoneValue.replace('zona-', '')}`;
+    } else if (zoneValue.includes('estado-')) {
+        value = `state/${zoneValue.replace('estado-', '')}`;
+    }
+
+    try {
+        const browser = await puppeteer.launch({
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath,
+            headless: chromium.headless,
+        });
+        console.log('Browser launched.');
+
+        const page = await browser.newPage();
+        console.log('New page created.');
+
+        await page.setRequestInterception(true);
+
+        const scrapedData = {
+            movies: null,
+            showtimes: null
+        };
+
+        page.on('request', request => {
+            if (request.resourceType() === 'fetch') {
+                console.log('Fetch Request:', request.url());
+            }
+            request.continue();
+        });
+
+        page.on('response', async response => {
+            const url = response.url();
+            const status = response.status();
+            const contentType = response.headers()['content-type'];
+
+            const targetUrls = [
+                `https://api.cinemex.com/rest/v2.25/cinemas/${value}/movies/?include_dates=1&initial=1`,
+                "https://api.cinemex.com/rest/v2.25/movies/"
+            ];
+
+            if (targetUrls.includes(url) && status === 200 && contentType && contentType.includes('application/json')) {
+                try {
+                    const data = await response.json();
+                    if (url.includes('/movies/?')) {
+                        scrapedData.showtimes = data;
+                    } else {
+                        scrapedData.movies = data;
+                    }
+
+                    console.log('Data fetched:', { url, data });
+
+                    // Check if both data have been fetched and save to Firestore
+                    if (scrapedData.movies && scrapedData.showtimes) {
+                        await saveToFirestore(scrapedData);
+                    }
+                } catch (error) {
+                    console.error(`Error saving response from ${url}:`, error);
+                }
+            }
+        });
+
+        await page.goto(`https://cinemex.com/cartelera/${zoneValue}/guadalajara`, { waitUntil: 'networkidle2' });
+
+        console.log('Page has been loaded. Responses from specified fetch requests will be saved.');
+
+        await browser.close();
+        res.status(200).send('Scraping completed successfully.');
+    } catch (error) {
+        console.error('Scraping error:', error);
+        res.status(500).send(`Scraping error: ${error}`);
+    }
 };
 
-// Crear un documento en la colección 'sessions'
-db.collection('sessions').doc('exampleSession').set(sessionData)
-  .then(() => {
-    console.log('Document successfully written!');
-    // Obtener el Documento
-    return db.collection('sessions').doc('exampleSession').get();
-  })
-  .then((doc) => {
-    if (doc.exists) {
-      console.log('Document data:', doc.data());
-      // Eliminar el Documento (opcional, si no deseas eliminarlo, puedes comentar estas líneas)
-      // return db.collection('sessions').doc('exampleSession').delete();
-    } else {
-      console.log('No such document!');
+async function saveToFirestore(data) {
+    try {
+        const docRef = await db.collection('sessions').add(data);
+        console.log(`Document successfully written with ID: ${docRef.id}`);
+    } catch (error) {
+        console.error('Error writing document to Firestore: ', error);
     }
-  })
-  .then(() => {
-    console.log('Document successfully deleted!'); // Este mensaje aparecerá si se elimina el documento
-  })
-  .catch((error) => {
-    console.error('Error: ', error);
-  });
+}
